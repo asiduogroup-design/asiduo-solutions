8dcb34b2c0bde350238d7433a3e03cb7654cd847
sravanthi <sravanthikamani@gmail.com>
2026-04-18 23:47:45 +0530

    background video updated

--- cilent/src/components/InteractiveHomePage.jsx
+++ cilent/src/components/InteractiveHomePage.jsx
@@ -70,15 +70,11 @@ export default function InteractiveHomePage({ locale = "en" }) {
           aria-hidden="true"
           className="absolute inset-0 h-full w-full object-cover"
         >
+          <source src="/videos/Cinematic_Workspace_Loop_Video.mp4" type="video/mp4" />
           <source
             src="/videos/Software_Electrical_Graphic_Design_Background_Video.mp4"
             type="video/mp4"
           />
-          <source
-            src="/videos/software_electrical_graphic_design_background_video.mp4"
-            type="video/mp4"
-          />
-          <source src="/videos/Cinematic_Workspace_Loop_Video.mp4" type="video/mp4" />
         </video>
 
         <div className="absolute inset-0 bg-black/70" />
